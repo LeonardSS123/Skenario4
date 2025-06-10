9970485ad1c10198827e65aecea61103c7ec7a3e
LeonardSS123 <mryorkeman@gmail.com>
2025-06-10 19:06:24 +0700

    Add files via upload

---
+++ skn4k2.php
@@ -0,0 +1,8 @@
+<?php
+for ($i = 30; $i > 1; $i-- ) {
+    echo "anak ayam turun $i, mati 1 tinggal ". ($i - 1);
+    echo "<br>";
+}
+echo "anak ayam turun 1, mati 1 tinggal induknya.";
+
+echo "<hr>";
